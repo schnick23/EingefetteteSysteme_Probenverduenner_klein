39a7e3b3c748195a1489e4e402aaae491a339e42
Malte Wendeler <malte.wendeler@provinzial.de>
2025-12-14 17:10:02 +0100

    cleanup dings

--- scripts/positionen_messen.py
+++ scripts/positionen_messen.py
@@ -9,6 +9,7 @@ import RPi.GPIO as GPIO
 
 def measure_positions(element):
     element.home()
+    setup_gpio()
     steps = 0
     while True:
         command = input("Drücke 'q' um abzubrechen, 'i' für aktuelle steps, 'w' zum vorwärts fahren, 's' zum zurück fahren: ")
@@ -22,6 +23,7 @@ def measure_positions(element):
                 front_steps = int(front_steps)
                 steps += front_steps
                 element.AXIS._do_step(front_steps, True)
+                setup_gpio()
             except ValueError:
                 print("Ungültige Eingabe. Bitte eine Zahl eingeben.")
         elif command.lower() == 'i':
@@ -32,6 +34,7 @@ def measure_positions(element):
                 back_steps = int(back_steps)
                 steps -= back_steps
                 element.AXIS._do_step(back_steps, False)
+                setup_gpio()
             except ValueError:
                 print("Ungültige Eingabe. Bitte eine Zahl eingeben.")
         
@@ -40,66 +43,63 @@ def measure_positions(element):
 GPIO.cleanup()
 print("GPIO cleaned up.")
 
-try:
-    with open('config.json', 'r') as f:
-        config = json.load(f)
+def setup_gpio():
+    try:
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        with open('config.json', 'r') as f:
+            config = json.load(f)
 
-    end_stop_pin = config['gpio']['endstops']['hub']
-    hub_step_pin = config['gpio']["stepper_motors"]['hub']['step_pin']
-    hub_en_pin = config['gpio']["stepper_motors"]['hub']['en_pin']
-    hub_dir_pin = config['gpio']["stepper_motors"]['hub']['dir_pin']
-    hub_axis = motorcontroller.Axis(
-        name="Hubtisch-Achse",
-        pin_step=hub_step_pin,
-        pin_dir=hub_dir_pin,
-        pin_en=hub_en_pin
-    )
-    hub_tisch = HubTisch.Hubtisch(
-        AXIS=hub_axis,
-        endstop_pin=end_stop_pin
-    )
+        end_stop_pin = config['gpio']['endstops']['hub']
+        hub_step_pin = config['gpio']["stepper_motors"]['hub']['step_pin']
+        hub_en_pin = config['gpio']["stepper_motors"]['hub']['en_pin']
+        hub_dir_pin = config['gpio']["stepper_motors"]['hub']['dir_pin']
+        hub_axis = motorcontroller.Axis(
+            name="Hubtisch-Achse",
+            pin_step=hub_step_pin,
+            pin_dir=hub_dir_pin,
+            pin_en=hub_en_pin
+        )
+        hub_tisch = HubTisch.Hubtisch(
+            AXIS=hub_axis,
+            endstop_pin=end_stop_pin
+        )
 
-    measure_positions(hub_tisch)
+        lin_axis = motorcontroller.Axis(
+            name="Linear-Achse",
+            pin_step=config['gpio']["stepper_motors"]['linear']['step_pin'],
+            pin_dir=config['gpio']["stepper_motors"]['linear']['dir_pin'],
+            pin_en=config['gpio']["stepper_motors"]['linear']['en_pin']
+        )
+        end_stop_pin_vorne = config['gpio']['endstops']['linear_vorne']
+        end_stop_pin_hinten = config['gpio']['endstops']['linear_hinten']
+        linear_fuehrung = LinearFuehrung.LinearFuehrung(
+            axis=lin_axis,
+            endstop_pin_vorne=end_stop_pin_vorne,
+            endstop_pin_hinten=end_stop_pin_hinten
+        )
 
-    lin_axis = motorcontroller.Axis(
-        name="Linear-Achse",
-        pin_step=config['gpio']["stepper_motors"]['linear']['step_pin'],
-        pin_dir=config['gpio']["stepper_motors"]['linear']['dir_pin'],
-        pin_en=config['gpio']["stepper_motors"]['linear']['en_pin']
-    )
-    end_stop_pin_vorne = config['gpio']['endstops']['linear_vorne']
-    end_stop_pin_hinten = config['gpio']['endstops']['linear_hinten']
-    linear_fuehrung = LinearFuehrung.LinearFuehrung(
-        axis=lin_axis,
-        endstop_pin_vorne=end_stop_pin_vorne,
-        endstop_pin_hinten=end_stop_pin_hinten
-    )
+        syr_axis = motorcontroller.Axis(
+            name="Spritzkopf-Achse",
+            pin_step=config['gpio']["stepper_motors"]['syringe']['step_pin'],
+            pin_dir=config['gpio']["stepper_motors"]['syringe']['dir_pin'],
+            pin_en=config['gpio']["stepper_motors"]['syringe']['en_pin']
+        )
+        end_stop_pin_links = config['gpio']['endstops']['syringe_links']
+        end_stop_pin_rechts = config['gpio']['endstops']['syringe_rechts']
+        spritzkopf = Spritzkopf.SyringeHead(
+            axis=syr_axis,
+            steps_per_ml=2000.0,  # Beispielwert, anpassen!
+            max_volume_ml=10.0,   # Beispielwert, anpassen!
+            draw_towards_positive=True,
+            start_volume_ml=0.0,
+            endstop_pin_links=end_stop_pin_links,
+            endstop_pin_rechts=end_stop_pin_rechts
+        )
+    
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("❌ Manuell abgebrochen.")
 
-    measure_positions(linear_fuehrung)
 
-    syr_axis = motorcontroller.Axis(
-        name="Spritzkopf-Achse",
-        pin_step=config['gpio']["stepper_motors"]['syringe']['step_pin'],
-        pin_dir=config['gpio']["stepper_motors"]['syringe']['dir_pin'],
-        pin_en=config['gpio']["stepper_motors"]['syringe']['en_pin']
-    )
-    end_stop_pin_links = config['gpio']['endstops']['syringe_links']
-    end_stop_pin_rechts = config['gpio']['endstops']['syringe_rechts']
-    spritzkopf = Spritzkopf.SyringeHead(
-        axis=syr_axis,
-        steps_per_ml=2000.0,  # Beispielwert, anpassen!
-        max_volume_ml=10.0,   # Beispielwert, anpassen!
-        draw_towards_positive=True,
-        start_volume_ml=0.0,
-        endstop_pin_links=end_stop_pin_links,
-        endstop_pin_rechts=end_stop_pin_rechts
-    )
 
-    measure_positions(spritzkopf)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("❌ Manuell abgebrochen.")
-
-finally:
-    GPIO.cleanup()
-    print("GPIO cleaned up.")
